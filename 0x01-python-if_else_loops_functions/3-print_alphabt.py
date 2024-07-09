bddaa91b1964e2e69de5b746ed635dd8707de168
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-07-09 16:06:50 -0700

    ASCII alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for letter in range(97,123):
-    if letter == 101 or  letter == 113:
+for letter in range(97, 123):
+    if letter == 101 or letter == 113:
         continue
     print("{}".format(chr(letter)), end="")
