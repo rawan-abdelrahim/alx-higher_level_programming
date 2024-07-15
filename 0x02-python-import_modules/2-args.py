5c18560b93fc0396ec4b305de236c7aac378657f
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-07-14 21:29:26 -0700

    print the argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,10 +8,9 @@ if __name__ == "__main__":
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 arguments.")
+        print("1 arguments:")
     else:
         print("{} arguments:".format(count))
 
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
