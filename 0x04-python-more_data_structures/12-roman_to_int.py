f3d258b7ddaae9cc63e508d0d3d5a8dac1fd8ddd
rawan-abdelrahim <rawanabdo7575@gmail.com>
2024-07-17 09:39:50 -0700

    Technical interview preparation

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+
+    result = 0
+
+    for i in range(len(roman_string) - 1):
+        if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+            result -= roman_dict[roman_string[i]]
+        else:
+            result += roman_dict[roman_string[i]]
+    
+    result += roman_dict[roman_string[len(roman_string) - 1]]
+
+    return (result)
